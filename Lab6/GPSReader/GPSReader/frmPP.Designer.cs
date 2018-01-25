d85a6a82287c01050fb7b5218421770e468e0cf1
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-01-25 22:02:36 +0100

    Add lab6 task solution

---
+++ Lab6/GPSReader/GPSReader/frmPP.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace GPSReader
+{
+    partial class frmPP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtLong = new System.Windows.Forms.TextBox();
+            this.txtLat = new System.Windows.Forms.TextBox();
+            this.btnMapIt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(89, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtLong
+            // 
+            this.txtLong.Location = new System.Drawing.Point(99, 120);
+            this.txtLong.Name = "txtLong";
+            this.txtLong.Size = new System.Drawing.Size(100, 20);
+            this.txtLong.TabIndex = 1;
+            // 
+            // txtLat
+            // 
+            this.txtLat.Location = new System.Drawing.Point(99, 71);
+            this.txtLat.Name = "txtLat";
+            this.txtLat.Size = new System.Drawing.Size(100, 20);
+            this.txtLat.TabIndex = 2;
+            // 
+            // btnMapIt
+            // 
+            this.btnMapIt.Location = new System.Drawing.Point(89, 218);
+            this.btnMapIt.Name = "btnMapIt";
+            this.btnMapIt.Size = new System.Drawing.Size(85, 23);
+            this.btnMapIt.TabIndex = 3;
+            this.btnMapIt.Text = "Show on map";
+            this.btnMapIt.UseVisualStyleBackColor = true;
+            this.btnMapIt.Click += new System.EventHandler(this.btnMapIt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Latitude";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Longitude";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(99, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Change COM Port";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "COM Port";
+            // 
+            // frmPP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnMapIt);
+            this.Controls.Add(this.txtLat);
+            this.Controls.Add(this.txtLong);
+            this.Controls.Add(this.button1);
+            this.Name = "frmPP";
+            this.Text = "GPS Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLong;
+        private System.Windows.Forms.TextBox txtLat;
+        private System.Windows.Forms.Button btnMapIt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Lab6/GPSReader/GPSReader/frmPP.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace GPSReader
+{
+    public partial class frmPP : Form
+    {
+        public string Latitude;
+        public string Longitude;
+
+        public frmPP()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            timer1.Start();
+            btnMapIt.Enabled = false;
+            textBox1.Text = "COM12";
+
+            // Try to open the serial port
+            try
+            {
+                serialPort1.PortName = "COM12";
+                serialPort1.Open();
+                button1.Text = "Stop Updates";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                timer1.Enabled = false;
+                button1.Text = "Update";
+                return;
+            }
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Try to update present position if the port is setup correctly
+        /// and the GPS device is returning values
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="sender"></param></span>
+        /// <span class="code-SummaryComment"><param name="e"></param></span>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                string data = serialPort1.ReadExisting();
+                string[] strArr = data.Split('$');
+                for (int i = 0; i < strArr.Length; i++)
+                {
+                    string strTemp = strArr[i];
+                    string[] lineArr = strTemp.Split(',');
+                    if (lineArr[0] == "GPGGA")
+                    {
+                        try
+                        {
+                            //Latitude
+                            Double dLat = Convert.ToDouble(lineArr[2]);
+                            dLat = dLat / 100;
+                            string[] lat = dLat.ToString().Split('.');
+                            Latitude = lineArr[3].ToString() +
+                            lat[0].ToString() + "." +
+                            ((Convert.ToDouble(lat[1]) /
+                            60)).ToString("#####");
+                            Latitude = lineArr[3].ToString() + dLat.ToString();
+
+
+                            //Longitude
+                            Double dLon = Convert.ToDouble(lineArr[4]);
+                            dLon = dLon / 100;
+                            string[] lon = dLon.ToString().Split('.');
+                            Longitude = lineArr[5].ToString() +
+                            lon[0].ToString() + "." +
+                            ((Convert.ToDouble(lon[1]) /
+                            60)).ToString("#####");
+                            Longitude = lineArr[5].ToString() + dLon.ToString();
+
+                            //Display
+                            txtLat.Text = Latitude;
+                            txtLong.Text = Longitude;
+
+                            btnMapIt.Enabled = true;
+                        }
+                        catch
+                        {
+                            //Cannot Read GPS values
+                            txtLat.Text = "GPS Unavailable";
+                            txtLong.Text = "GPS Unavailable";
+                            btnMapIt.Enabled = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                txtLat.Text = "COM Port Closed";
+                txtLong.Text = "COM Port Closed";
+                btnMapIt.Enabled = false;
+            }
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Enable or disable the timer to start continuous
+        /// updates or disable all updates
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="sender"></param></span>
+        /// <span class="code-SummaryComment"><param name="e"></param></span>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+
+            if (button1.Text == "Update")
+            {
+                button1.Text = "Stop Updates";
+                txtLat.Text = "";
+                txtLong.Text = "";
+            }
+            else
+            {
+                button1.Text = "Update";
+                txtLat.Text = "Updates stopped";
+                txtLong.Text = "Updates stopped";
+            }
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Exit the application
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="sender"></param></span>
+        /// <span class="code-SummaryComment"><param name="e"></param></span>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        /// <span class="code-SummaryComment"><summary></span>
+        /// Open a map of the present position
+        /// <span class="code-SummaryComment"></summary></span>
+        /// <span class="code-SummaryComment"><param name="sender"></param></span>
+        /// <span class="code-SummaryComment"><param name="e"></param></span>
+        private void btnMapIt_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                StringBuilder queryAddress = new StringBuilder();
+                queryAddress.Append("http://maps.google.com/maps?q=");
+
+                if (Latitude != string.Empty)
+                {
+                    queryAddress.Append(Latitude + "%2C");
+                }
+
+                if (Longitude != string.Empty)
+                {
+                    queryAddress.Append(Longitude);
+                }
+
+                System.Diagnostics.Process.Start(queryAddress.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.Close();
+                serialPort1.PortName = textBox1.Text;
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                timer1.Enabled = false;
+                button1.Text = "Update";
+                return;
+            }
+        }
+    }
+}
